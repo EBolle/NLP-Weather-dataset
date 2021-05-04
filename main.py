8654ee288ad074e2f48e3ee0efb9632d2cdc5a40
EBolle <ernstbolle@gmail.com>
2021-05-04 21:07:15 +0200

    Simplified main.py, ready for a submit spark job test

--- main.py
+++ main.py
@@ -5,24 +5,16 @@ import configparser
 from haverstine_distance import udf_get_distance
 from pyspark.sql import SparkSession
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.functions import (avg, col, concat_ws, broadcast, first, substring, to_date, year)
-from pyspark.sql.types import DoubleType, IntegerType, StructType
+from pyspark.sql.functions import avg, col, concat_ws, broadcast, first, substring, to_date, year
+from pyspark.sql.types import DoubleType, IntegerType
 from schemas import yearly_weather_schema
 
 config = configparser.ConfigParser()
 config.read('settings.cfg')
 
-business_path = config['PATHS']['business']
-us_stations_path = config['PATHS']['us_stations']
-max_distance = int(config['PARAMETERS']['max_distance'])
-
-review_path = config['PATHS']['review']
-user_path = config['PATHS']['user']
-
-yearly_weather_path = config['PATHS']['yearly_weather']
 elements_to_keep = config['PARAMETERS']['weather_elements']
-
-output_path = config['PATHS']['output']
+max_distance = int(config['PARAMETERS']['max_distance'])
+s3_bucket = config['AWS']['s3_bucket']
 
 
 def main():
@@ -30,10 +22,10 @@ def main():
     """
     spark = create_spark_session()
 
-    distances = create_distances(spark, business_path, us_stations_path, max_distance)
-    review = create_review(spark, review_path, user_path)
-    yearly_weather = create_yearly_weather(spark, yearly_weather_path, yearly_weather_schema, elements_to_keep)
-    final_table = create_final_table()
+    distances = create_distances(spark)
+    review = create_review(spark)
+    yearly_weather = create_yearly_weather(spark)
+    final_table = create_final_table(distances, review, yearly_weather)
 
     write_final_table(final_table)
 
@@ -44,7 +36,7 @@ def create_spark_session():
     """Gets or creates an activate Spark session."""
     spark = (SparkSession
         .builder
-        .appName("Sparkify ETL")
+        .appName("NLP-Weather-dataset ETL")
         .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0")
         .getOrCreate()
     )
@@ -62,6 +54,8 @@ def create_distances(spark, business_path: str, us_stations_path: str, max_dista
     Returns:
 
     """
+    business_path = f"s3://{s3_bucket}/yelp/business.json.gz"
+
     business = (spark
         .read
         .json(business_path)
@@ -74,6 +68,8 @@ def create_distances(spark, business_path: str, us_stations_path: str, max_dista
                 col('latitude'),
                 col('longitude'))
     )
+
+    us_stations_path = f"s3://{s3_bucket}/ghcn/us_stations.txt.gz"
 
     us_stations = (spark
         .read
@@ -112,7 +108,7 @@ def create_distances(spark, business_path: str, us_stations_path: str, max_dista
     return distances
 
 
-def create_review(spark, review_path: str, user_path: str) -> DataFrame:
+def create_review(spark) -> DataFrame:
     """
     Subsets the raw review data based on 2 filters:
     - the business must be open
@@ -123,12 +119,16 @@ def create_review(spark, review_path: str, user_path: str) -> DataFrame:
     Returns:
 
     """
+    user_path = f"s3://{s3_bucket}/yelp/user.json.gz"
+
     user = (spark
         .read
         .json(user_path)
         .filter(col('useful') > 25)
         .select(col('user_id'))
     )
+
+    review_path = f"s3://{s3_bucket}/yelp/review.json.gz"
 
     review_raw = (spark
         .read
@@ -153,10 +153,7 @@ def create_review(spark, review_path: str, user_path: str) -> DataFrame:
     return review
 
 
-def create_yearly_weather(spark,
-                          yearly_weather_path: str,
-                          yearly_weather_schema: StructType,
-                          elements_to_keep: list) -> DataFrame:
+def create_yearly_weather(spark) -> DataFrame:
     """
     Reads in 18 years of daily weather reports throughout the world. After filtering on US stations only, and keeping
     only the most prevalent key weather metrics, the dataframe needs to be pivotted so it can be easily joined with
@@ -166,6 +163,8 @@ def create_yearly_weather(spark,
 
     Returns:
     """
+    yearly_weather_path = f"s3://{s3_bucket}/ghcn/year_2020.csv.gz"
+
     yearly_weather = (spark
         .read
         .csv(yearly_weather_path, header=False, schema=yearly_weather_schema)
@@ -202,37 +201,39 @@ def create_final_table(distances: DataFrame, review: DataFrame, yearly_weather_p
     distance_review_join_condition = [distances.business_id == review.business_id]
 
     distances_review = (distances
-                        .join(review, on=distance_review_join_condition, how='inner')
-                        .select(col('city'),
-                                col('state'),
-                                col('text'),
-                                col('review_date'),
-                                col('us_station_id'))
-                        )
+        .join(review, on=distance_review_join_condition, how='inner')
+        .select(col('city'),
+                col('state'),
+                col('text'),
+                col('review_date'),
+                col('us_station_id'))
+    )
 
     distances_review_weather_join_condition = [distances_review.review_date == yearly_weather_pivot.weather_date,
                                                distances_review.us_station_id == yearly_weather_pivot.station_id]
 
     final_table = (distances_review
-                   .join(yearly_weather_pivot, on=distances_review_weather_join_condition, how='inner')
-                   .groupby('city', 'state', 'review_date', 'text')
-                   .agg(avg('PRCP'),
-                        avg('SNOW'),
-                        avg('SNWD'),
-                        avg('TMAX'),
-                        avg('TMIN'))
-                   )
+       .join(yearly_weather_pivot, on=distances_review_weather_join_condition, how='inner')
+       .groupby('city', 'state', 'review_date', 'text')
+       .agg(avg('PRCP'),
+            avg('SNOW'),
+            avg('SNWD'),
+            avg('TMAX'),
+            avg('TMIN'))
+    )
 
     return final_table
 
 
 def write_final_table(final_table: DataFrame):
     """Writes the final table back to S3, you can modify the exact location in settings.cfg"""
-    (final_table
-     .write
-     .option('compression', 'gzip')
-     .mode('overwrite')
-     .json(output_path)
+    output_path = f"s3://{s3_bucket}/output/final_table.json.gz"
+
+    final_table_write = (final_table
+         .write
+         .option('compression', 'gzip')
+         .mode('overwrite')
+         .json(output_path)
      )
 
 
