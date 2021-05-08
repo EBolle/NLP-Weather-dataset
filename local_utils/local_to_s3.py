f639d6b96b0b7e4664721ce888887c98118c13c8
EBolle <ernstbolle@gmail.com>
2021-05-08 13:58:51 +0200

    Updated the local_to_s3.py module with Bucket creation and a loop for the script and modules

--- local_utils/local_to_s3.py
+++ local_utils/local_to_s3.py
@@ -1,6 +1,6 @@
 """
-This module helps to upload the files from Yelp and GHCN to S3. It is recommended to gzip the files before uploading.
-You can set the folders with all your files in settings.cfg before running this script.
+This module helps to upload the data from Yelp and GHCN, and the Spark script and helper modules to S3. It is
+recommended to gzip the data files before uploading.
 """
 
 
@@ -14,24 +14,35 @@ config.read('settings.cfg')
 yelp_folder = Path(config['LOCAL_PATHS']['yelp_folder'])
 ghcn_folder = Path(config['LOCAL_PATHS']['ghcn_folder'])
 s3_bucket = config['AWS']['s3_bucket']
+s3_location = config['AWS']['s3_location']
 
-yelp_paths = [path for path in yelp_folder.iterdir()]
-ghcn_paths = [path for path in ghcn_folder.iterdir()]
+yelp_files = [path for path in yelp_folder.iterdir()]
+ghcn_files = [path for path in ghcn_folder.iterdir()]
+cwd = Path('.')
+spark_files = [cwd / 'main.py',
+               cwd / 'haversine_distance.py',
+               cwd / 'schemas.py']
+
 
 client = boto3.client('s3')
 
-for path in yelp_paths:
-    client.upload_file(Filename=str(path), Bucket=s3_bucket, Key=f'yelp/{path.name}')
+response = client.create_bucket(
+    ACL='private',
+    Bucket=s3_bucket,
+    CreateBucketConfiguration={
+        'LocationConstraint': s3_location
+    },
+)
 
-for path in ghcn_paths:
-    client.upload_file(Filename=str(path), Bucket=s3_bucket, Key=f'ghcn/{path.name}')
+for file in yelp_files:
+    client.upload_file(Filename=str(file), Bucket=s3_bucket, Key=f'yelp/{file.name}')
 
-# Scripts should be uploaded as well, so first git clone, then run this script ..
-# When all the json files are in S3, create another scripts s3_to_local to create 1 json file in Pandas :).
-# Haversine is not known on the other nodes, you have to pass it to these nodes explicitly with spark submit --py-files
-# https://stackoverflow.com/questions/57315030/aws-emr-modulenotfounderror-no-module-named-pyarrow
-# repartition lijkt toch wel erg de moeite waard te zijn, 1 executor is nog lange tijd bezig.. :)
-# check verdeling in notebook?
+for file in ghcn_files:
+    client.upload_file(Filename=str(file), Bucket=s3_bucket, Key=f'ghcn/{file.name}')
+
+for file in spark_files:
+    client.upload_file(Filename=str(file), Bucket=s3_bucket, Key=f'spark/{file.name}')
+
 # [hadoop@ip-172-31-24-15 spark_app]$ spark-submit --master yarn --conf spark.dynamicAllocation.enabled=true --py-files haversine.py main.py
 # Script doet er ongeveer 25 min over, maar check de output nog, aantal partitions is anders op het einde, werk aan de winkel :)
 # Check output!
